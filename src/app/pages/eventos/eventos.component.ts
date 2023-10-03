1e2f66add80e10f80df0aceaadd0a6ac551eb4d8
Joao Passos <joao.passos@invillia.com>
2023-10-03 11:18:17 -0300

    incluido card com bebidas

--- src/app/pages/eventos/eventos.component.ts
+++ src/app/pages/eventos/eventos.component.ts
@@ -8,16 +8,36 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class EventosComponent implements OnInit {
   public eventoForm: FormGroup;
+  public bebidasDoEvento: any[] = [];
+
+  public bebidasDisponiveis = [
+    { nome: 'Bebida 1' },
+    { nome: 'Bebida 2' },
+  ];
 
   constructor(private fb: FormBuilder) {
     this.eventoForm = this.fb.group({
       nomeEvento: ['', Validators.required],
       dataEvento: ['', Validators.required],
-      localEvento: ['', Validators.required]
+      localEvento: ['', Validators.required],
+      bebidaSelecionada: ['', Validators.required],
+      quantidadeBebida: ['', Validators.required]
     });
+
+    this.bebidasDoEvento = [];
   }
 
   ngOnInit() {}
+
+  adicionarBebida() {
+    const bebidaSelecionada = this.eventoForm.get('bebidaSelecionada')?.value;
+    const quantidadeBebida = this.eventoForm.get('quantidadeBebida')?.value;
+
+    if (bebidaSelecionada && quantidadeBebida) {
+      const bebida = { nome: bebidaSelecionada, quantidade: quantidadeBebida };
+      this.bebidasDoEvento.push(bebida);  // Crie um array bebidasDoEvento no componente para armazenar as bebidas
+    }
+  }
 
   salvarEvento() {
     console.log('Evento salvo:', this.eventoForm.value);
