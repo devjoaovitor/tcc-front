0fdfdceb8e107faa3d24a077383786007a9853c2
Joao Passos <joao.passos@invillia.com>
2024-03-21 16:56:11 -0300

    a

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,8 +7,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class AuthService {
-  // private apiUrl = 'http://localhost:3000/api/auth';
-  private apiUrl = 'https://tcc-back-a4qz01zcp-devjoaovitors-projects.vercel.app/api/auth';
+  private apiUrl = 'https://tcc-back-one.vercel.app/api/auth';
 
   constructor(private http: HttpClient, private router: Router) {}
 
